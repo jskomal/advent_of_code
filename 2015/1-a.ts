35d9b180d5619b82f5f500448999a3a87d4d679a
jskomal <90876852+jskomal@users.noreply.github.com>
2022-12-01 14:38:01 -0700

    fix file pathing

--- 2015/1-a.ts
+++ 2015/1-a.ts
@@ -1,14 +1,14 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const day1A = async () => {
-  const fileData = await fs.readFileSync('./1.txt')
-  const orders = fileData.toString().split('')
+	const fileData = await fs.readFileSync("./2015/1.txt");
+	const orders = fileData.toString().split("");
 
-  let count = 0
-  for (let i = 0; i < orders.length; i++) {
-    orders[i] === '(' ? count++ : count--
-  }
-  console.log(count)
-  return count
-}
-day1A()
+	let count = 0;
+	for (let i = 0; i < orders.length; i++) {
+		orders[i] === "(" ? count++ : count--;
+	}
+	console.log(count);
+	return count;
+};
+day1A();
--- 2015/1-b.ts
+++ 2015/1-b.ts
@@ -1,16 +1,16 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const day2A = async () => {
-  const fileData = await fs.readFileSync('./1.txt')
-  const orders = fileData.toString().split('')
+	const fileData = await fs.readFileSync("./2015/1.txt");
+	const orders = fileData.toString().split("");
 
-  let floor = 0
-  let i = 0
-  for (i; i < orders.length; i++) {
-    if (floor === -1) break
-    orders[i] === '(' ? floor++ : floor--
-  }
-  console.log(i)
-  return i
-}
-day2A()
+	let floor = 0;
+	let i = 0;
+	for (i; i < orders.length; i++) {
+		if (floor === -1) break;
+		orders[i] === "(" ? floor++ : floor--;
+	}
+	console.log(i);
+	return i;
+};
+day2A();
